3147a6ae227f8d195af155476443093f2e094709
Дмитрий Троицкий <dimatroickij@yandex.ru>
2020-11-12 12:06:42 +0300

    Исправлена модификация сборщика.

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,6 @@ ssh -o StrictHostKeyChecking=no dimatroickij@$IP_ADDRESS << 'ENDSSH'
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $IMAGE:web
-  docker pull $IMAGE:nginx
   docker pull library/rabbitmq:3.8-management
-  docker pull $IMAGE:celery
-  docker pull $IMAGE:celery_beat
   docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
