88a9d07ed895eb6c69098691e7fa817ca66d869b
dimatroickij <dimatroickij@yandex.ru>
2020-12-23 10:56:24 +0300

    Изменение названий контейнеров для единобразия всех контейнеров на сервере

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ssh -o StrictHostKeyChecking=no dimatroickij@$IP_ADDRESS << 'ENDSSH'
-  cd /home/dimatroickij/app
+  cd /home/dimatroickij/applications/smarthome
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-  docker pull $IMAGE:web
-  docker pull $IMAGE:nginx
+  docker pull $IMAGE:smarthome
+  docker pull $IMAGE:smarthome-nginx
   docker pull library/rabbitmq:3.8-management
   docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
