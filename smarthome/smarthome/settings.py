f46d9d05f96bbd6feaa71e4035f08b44664ea918
dimatroickij <dimatroickij@yandex.ru>
2020-12-26 17:06:38 +0300

    исправлены конфигурации для статики

--- smarthome/smarthome/settings.py
+++ smarthome/smarthome/settings.py
@@ -129,6 +129,7 @@ SITE_URL = os.environ.get('SITE_URL')
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
