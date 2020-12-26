a778c243aaf08d06b3fffd528ad6a36b5262c612
dimatroickij <dimatroickij@yandex.ru>
2020-12-26 21:43:08 +0300

    Добавление конфигурации для виртуального хоста nginx-proxy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh -o StrictHostKeyChecking=no dimatroickij@$IP_ADDRESS << 'ENDSSH'
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $IMAGE:smarthome
-  docker pull $IMAGE:smarthome-nginx
+#  docker pull $IMAGE:smarthome-nginx
   docker pull library/rabbitmq:3.8-management
   docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
